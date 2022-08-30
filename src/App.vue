b73217061023e5d906e805d92b737e3d6b591785
Ewerton Franco <ewerton.silva@usemobile.xyz>
2022-08-30 17:30:38 -0300

    created a form to add new todos

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div>
     <h1>Lista de tarefas</h1>
 
+    <to-do-form @todo-added="addToDo" />
+
     <ul>
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item :id="item.id" :label="item.label" :done="item.done" />
@@ -13,11 +15,13 @@
 <script>
 import uniqueid from 'lodash.uniqueid'
 
+import ToDoForm from './components/ToDoForm.vue'
 import ToDoItem from './components/ToDoItem.vue'
 
 export default {
   name: 'App',
   components: {
+    ToDoForm,
     ToDoItem,
   },
   data() {
@@ -37,6 +41,15 @@ export default {
         },
       ],
     }
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      this.ToDoItems.push({
+        id: uniqueid('todo-'),
+        label: toDoLabel,
+        done: false,
+      })
+    },
   },
 }
 </script>
---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <label for="new-todo-input"> Qual a próxima tarefa? </label>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off"
+      v-model.lazy.trim="label"
+    />
+    <button type="submit">Adicionar</button>
+  </form>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSubmit() {
+      if (this.label === undefined || this.label === '') return
+      this.$emit('todo-added', this.label)
+    },
+    data() {
+      return {
+        label: '',
+      }
+    },
+  },
+}
+</script>
